6977d6593a2c403013ac78f5e3138060f735eb4b
vas-chif <vasile.chifeac@gmail.com>
2024-03-05 05:55:35 +0100

    feat: backend working :)

--- README.md
+++ README.md
@@ -2,4 +2,99 @@
 
 This Is a simple fullstack random number generator program developed by @chif-vas(me) during the node and typesript dvelopment course.
 
-## In this project i create a panel where is show the extract numbers of a lottery in more cities of the Italy
+In this project i create a panel where is show the extract numbers of a lottery in more cities of the Italy
+
+## Install
+
+'''bash
+npm install
+'''
+
+## Usage backend
+
+'''bash
+ts-node main.ts
+'''
+
+example output:
+'''
+{
+"Bari": [
+51,
+43,
+56,
+98,
+87
+],
+"Cagliari": [
+81,
+85,
+26,
+64,
+48
+],
+"Firenze": [
+24,
+14,
+36,
+45,
+92
+],
+"Genova": [
+70,
+49,
+21,
+45,
+83
+],
+"Milano": [
+7,
+48,
+75,
+31,
+76
+],
+"Napoli": [
+37,
+78,
+39,
+97,
+43
+],
+"Palermo": [
+60,
+35,
+52,
+17,
+96
+],
+"Roma": [
+27,
+36,
+58,
+49,
+24
+],
+"Torino": [
+35,
+3,
+96,
+77,
+73
+],
+"Venezia": [
+19,
+33,
+44,
+92,
+21
+],
+"Nazionale": [
+33,
+99,
+42,
+54,
+10
+]
+}
+'''
--- main.ts
+++ main.ts
@@ -1,1 +1,48 @@
+import { log } from "console";
 
+function RNG(min: number, max: number) {
+  const rng = Math.random();
+  /* rng * (max -min) +min
+     rng = 0 => min
+     rng = 1 => max   */
+  return Math.trunc(rng * (max - min) + min);
+}
+
+function RNGSequence(len: number, min: number, max: number) {
+  if (len > max - min) {
+    throw new Error(
+      "Cannot find " + len + " numbers between " + min + " and " + max
+    );
+  }
+  const res: number[] = [];
+  while (res.length < len) {
+    const rn = RNG(min, max);
+    if (res.includes(rn)) {
+      continue;
+    }
+    res.push(rn);
+  }
+  return res;
+}
+
+const ruote = [
+  "Bari",
+  "Cagliari",
+  "Firenze",
+  "Genova",
+  "Milano",
+  "Napoli",
+  "Palermo",
+  "Roma",
+  "Torino",
+  "Venezia",
+  "Nazionale",
+];
+const estrazioni: { [ruota: string]: number[] } = {};
+
+for (const ruota of ruote) {
+  const estrazione = RNGSequence(5, 0, 100);
+  estrazioni[ruota] = estrazione;
+}
+
+console.log(JSON.stringify(estrazioni, null, 2));
